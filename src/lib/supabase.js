8a4a27b06e111ba8b55278fd9fa4b92b7f720468
Seba0803 <devecchisebastiano@gmail.com>
2026-03-16 23:40:38 +0100

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://xjdansvutcphqeedmjgp.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_0fdYtlHpQS89ucRSFQgf-A_zsML2tsQ.' // la tua chiave completa
+const SUPABASE_ANON_KEY = 'sb_publishable_0fdYtlHpQS89ucRSFQgf-A_zsML2tsQ' // la tua chiave completa
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: { persistSession: false }
